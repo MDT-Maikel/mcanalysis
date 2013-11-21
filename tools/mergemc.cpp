db5a93eae80e2c394037bb3c933487d51d19d6e7
Maikel de Vries <mdt.maikel@gmail.com>
2013-11-21 14:13:06 +0100

    added mergemc program

---
+++ tools/mergemc.cpp
@@ -0,0 +1,33 @@
+/* Merge MC
+ *
+ * 
+*/
+
+#include <iostream>
+
+#include "../source/utility/utility.h"
+#include "../source/event/event.h"
+#include "boost/filesystem.hpp"
+
+
+using namespace std;
+using namespace analysis;
+using namespace boost::filesystem;
+
+
+int main(int argc, char* argv[])
+{
+	
+	vector<path> files(get_files("files/", ".lhco.gz", true));
+	
+	for (unsigned int i = 0; i < files.size(); i++)
+		cout << files[i].string() << endl;
+	
+	vector<event*> events;
+	for (unsigned int i = 0; i < files.size(); i++)
+		read_lhco(events, files[i]);
+
+	write_lhco(events, "./merged.lhco.gz");
+
+	return 0;
+}
