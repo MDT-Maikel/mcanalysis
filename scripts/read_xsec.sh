7b665b4e79211f26460381689b6149d9dd29c191
Maikel de Vries <mdt.maikel@gmail.com>
2014-02-21 11:52:11 +0100

    added script to read madgraph cross sections

---
+++ scripts/read_xsec.sh
@@ -0,0 +1,24 @@
+# script to read the cross section from a madgraph file
+
+#!/bin/sh
+
+# print usage, if no argument is specified
+if [ $# -eq 0 ] 
+then
+    echo "Usage: $0 <file>"
+    exit 1
+fi
+
+# get file and check whether it exists
+xsec_file=$1
+if [ ! -e $xsec_file ]; then
+	echo "$0: Error opening file $3" 
+	exit 2
+fi   
+
+# get the cross section using the identifier and strip useless characters.
+xsec=$( awk '/^#  Integrated weight/ { $1=""; print $0; exit}' $xsec_file )
+printf -v xsec '%s' $xsec
+echo "$xsec" | tr -d [:alpha:] | tr -d '(' | tr -d ')' | tr -d ':'
+
+exit
